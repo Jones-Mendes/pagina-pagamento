bba38028d463e67c4c82353da986d759d0e448f8
Jones-Mendes <jonesoliver99@hotmail.com>
2025-04-29 11:52:38 -0300

    Continuação do pagina

---
+++ src/components/BackCard/index.jsx
@@ -0,0 +1,14 @@
+export default function BackCard(){
+    return (
+        <div className="w-[500px] h-[300px] bg-black rounded-xl">
+            <div className="w-full h-[20%] flex items-end">
+                <div className="w-full h-[50px] bg-[#2F2F2F]"></div>
+            </div>
+            <div className="w-full h-[30%] flex justify-center pt-[30px]">
+                <div className="w-[60%] h-[50px] bg-[#AEB6BF] flex items-center justify-end">
+                    <p className="text-black text-[20px] mr-2">000</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/FrontCard/index.jsx
+++ src/components/FrontCard/index.jsx
@@ -5,7 +5,7 @@ export default function CardFront(){
     return(
         <div className="w-[500px] h-[300px] bg-black rounded-2xl">
             <div className="w-full h-[30%] flex">
-                <div className="w-[50%] h-full flex items-center pl-4 gap-2">
+                <div className="w-[60%] h-full flex items-center pl-4 gap-2">
                     <div className="w-[70px] h-[70px] rounded-full bg-white"></div>
                     <div className="w-[50px] h-[50px] rounded-full bg-white"></div>
                     
